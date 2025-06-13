d78b7327212e982741cfd50840202a2e085affc1
benmelz <ben@melz.me>
2025-06-13 17:06:08 -0400

    add optgroup example

--- index.js
+++ index.js
@@ -2,7 +2,5 @@ import 'bootstrap'
 import BootstrapPicker from './lib/index.js'
 
 document.addEventListener('DOMContentLoaded', () => {
-  const basicPicker = document.getElementById('basic-picker')
-  // eslint-disable-next-line no-new
-  new BootstrapPicker(basicPicker)
+  document.querySelectorAll('.picker').forEach((element) => new BootstrapPicker(element))
 })
